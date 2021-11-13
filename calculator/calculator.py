0937719101d3af6dc013268f197a0cdd56bf5ea8
sandeepajaladi <90530329+sandeepajaladi@users.noreply.github.com>
2021-11-13 10:32:03 -0800

    Update calculator.py

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -9,28 +9,31 @@ class Calculator:
     history = []
     @staticmethod
     def get_result_of_first_calculation_added_to_history():
+        """Result of first calculation added to history"""
         return Calculator.history[0].get_Result()
     @staticmethod
     def clear_history():
+        """Function to clear history"""
         Calculator.history.clear()
         return True
     @staticmethod
     def history_count():
+        """Function to count history"""
         return len(Calculator.history)
     @staticmethod
     def add_calculation_to_history(calculation):
+        """Function to add calculations"""
         Calculator.history.append(calculation)
         return True
     @staticmethod
     def get_result_of_last_calculation_added_to_history():
-        # -1 gets the last item added to the list automaticly and you can expect it to have the get result method
+        """Function to get last result from history"""
         return Calculator.history[-1].get_Result()
     @staticmethod
     def add_number(value_a, value_b):
         """ adds number to result"""
         #create an addition object using the factory we created on the calculation class
         addition = Addition.create(value_a,value_b)
-        # addition = Addition(value_a,value_b) <-this is not good but will work.  It will be repeated too much
         Calculator.add_calculation_to_history(addition)
         return Calculator.get_result_of_last_calculation_added_to_history()
     @staticmethod
@@ -39,12 +42,10 @@ class Calculator:
         """ subtract number from result"""
         # create an subtraction object using the factory we created on the calculation class
         subtraction = Subtraction.create(value_a, value_b)
-        # addition = Addition(value_a,value_b) <-this is not good but will work.  It will be repeated too much
         Calculator.add_calculation_to_history(subtraction)
         return Calculator.get_result_of_last_calculation_added_to_history()
     @staticmethod
     def multiply_numbers(value_a, value_b):
         """ multiply two numbers and store the result"""
-        #this is a shorthand way to create the multiplication object and added it the history in one line
         Calculator.add_calculation_to_history(Multiplication.create(value_a,value_b))
         return Calculator.get_result_of_last_calculation_added_to_history()
