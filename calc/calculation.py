2c97202b72755702a9d29cf356a76ac00cbe002e
sandeepajaladi <90530329+sandeepajaladi@users.noreply.github.com>
2021-11-13 10:43:20 -0800

    Update calculation.py

--- calc/calculation.py
+++ calc/calculation.py
@@ -1,5 +1,5 @@
 """This is our calculation base class / Abstract Class"""
-class Calculation():
+class Calculation:
     """Base class for calculation """
     #contstructor and it is the first function called when an object of the class is instantiated
     def __init__(self,value_a, value_b):
