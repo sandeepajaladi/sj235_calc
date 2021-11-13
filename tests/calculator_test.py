e0706295fc030e1b4d20b81b05fb3cd1990646d1
sandeepajaladi <90530329+sandeepajaladi@users.noreply.github.com>
2021-11-13 09:07:09 -0800

    Update calculator_test.py

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,39 +1,54 @@
 """Testing the Calculator"""
-from calculator.main import Calculator
+import pprint
 
-def test_calculator_add_result():
-    """testing calculator result is 0"""
-    calc = Calculator()
-    result = calc.add(1,2)
-    assert result == 3
+import pytest
 
-def test_calculator_subtract_result():
-    """testing calculator result is 0"""
-    calc = Calculator()
-    result = calc.subtract(1,2)
-    assert result == -1
+from calculator.calculator import Calculator
 
+#this is how you define a function that will run each time you pass it to a test, it is called a fixture
+@pytest.fixture
+def clear_history():
+    Calculator.clear_history()
 
-def test_calculator_multiply_result():
-    """testing calculator result is 0"""
-    calc = Calculator()
-    result = calc.multiply(1,2)
-    assert result == 2
+def test_calculator_add(clear_history):
+    """Testing the Add function of the calculator"""
+    assert Calculator.add_number(1,2) == 3
+    assert Calculator.add_number(2, 2) == 4
+    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.add_number(4, 2) == 6
+    assert Calculator.history_count() == 4
+    assert Calculator.get_result_of_last_calculation_added_to_history() == 6
+    pprint.pprint(Calculator.history)
 
-def test_calculator_divide_result():
-    """testing calculator result is 0"""
-    calc = Calculator()
-    result = calc.divide(1,2)
-    assert result == 0.5
+def test_clear_history(clear_history):
+    assert Calculator.add_number(1,2) == 3
+    assert Calculator.add_number(2, 2) == 4
+    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.add_number(4, 2) == 6
+    assert Calculator.history_count() == 4
+    assert Calculator.clear_history() == True
+    assert Calculator.history_count() == 0
 
-def test_calculator_divide_fail_result():
-    """testing calculator result is 0"""
-    calc = Calculator()
-    result = calc.divide(1,0)
-    assert result == "Error: A number cannot be divided by 0"
+def test_count_history(clear_history):
+    assert Calculator.history_count() == 0
+    assert Calculator.add_number(2, 2) == 4
+    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.history_count() == 2
 
-def test_calculator_increment_result():
-    """testing calculator result is 0"""
-    calc = Calculator()
-    result = calc.increment(2)
-    assert result == 3
+def test_get_last_calculation_result(clear_history):
+    assert Calculator.add_number(2, 2) == 4
+    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.get_result_of_last_calculation_added_to_history() == 5
+
+def test_get_first_calculation_result(clear_history):
+    assert Calculator.add_number(2, 2) == 4
+    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.get_result_of_first_calculation_added_to_history() == 4
+
+def test_calculator_subtract(clear_history):
+    """Testing the subtract method of the calculator"""
+    assert Calculator.subtract_number(1, 2) == -1
+
+def test_calculator_multiply(clear_history):
+    """ tests multiplication of two numbers"""
+    assert Calculator.multiply_numbers(1,2) == 2
