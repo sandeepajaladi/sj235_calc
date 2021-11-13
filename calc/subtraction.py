e0f1a79230d413da50910581424cd06e337aff06
sandeepajaladi <90530329+sandeepajaladi@users.noreply.github.com>
2021-11-13 10:26:37 -0800

    Update subtraction.py

--- calc/subtraction.py
+++ calc/subtraction.py
@@ -3,7 +3,7 @@
 from calc.calculation import Calculation
 
 class Subtraction(Calculation):
-        """Subtraction class has one method to get the result of A and B come from the parent class"""
+        """Subtraction class method will get the result of A and B from the parent class"""
         def get_result(self):
                 """Get result from calculation"""
                 return self.value_a - self.value_b
