9654cc96c3d63baffde96b484590c06ae40c51a7
sandeepajaladi <90530329+sandeepajaladi@users.noreply.github.com>
2021-11-13 09:33:07 -0800

    Update multiplication.py

--- calc/multiplication.py
+++ calc/multiplication.py
@@ -4,6 +4,6 @@ from calc.calculation import Calculation
 
 class Multiplication(Calculation):
     """Multiplication class has a method to get the result of the calculation from the parent class"""
-    def get_Result(self):
+    def get_result(self):
         """Returns result from the function"""
         return self.value_a * self.value_b
