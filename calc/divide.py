72a7fd2e1e0ebe6aa863ede4976e167aa5ad7027
sandeepajaladi <90530329+sandeepajaladi@users.noreply.github.com>
2021-11-13 18:03:33 -0800

    Create divide.py

---
+++ calc/divide.py
@@ -0,0 +1,12 @@
+"""Divide calculation that inherits the value A and value B from the calculation class"""
+
+from calc.calculation import Calculation
+
+class Divide(Calculation):
+    """Divide class has a method to get the result of calculation from the parent class"""
+    def get_result(self):
+        """Returns result from the function"""
+        if self.value_b <= 0
+          return "Error"
+        else 
+          return self.value_a / self.value_b
