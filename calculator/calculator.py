1859b186e25dab527f54ed23d5a6b9644900413b
sandeepajaladi <90530329+sandeepajaladi@users.noreply.github.com>
2021-11-13 18:01:22 -0800

    Update calculator.py

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -49,3 +49,8 @@ class Calculator:
         """ multiply two numbers and store the result"""
         Calculator.add_calculation_to_history(Multiplication.create(value_a,value_b))
         return Calculator.get_result_of_last_calculation_added_to_history()
+    @staticmethod
+    def divide_numbers(value_a, value_b):
+        """ Divide two numbers and store the result"""
+        Calculator.add_calculation_to_history(Divide.create(value_a,value_b))
+        return Calculator.get_result_of_last_calculation_added_to_history()
