f4e5c33e158bf5682dee7c7dad98372d9f2117ed
jaladis <sj235@njit.edu>
2021-11-15 17:26:07 -0500

    Updating  branch for assignment4

---
+++ calc/calculations/divide.py
@@ -0,0 +1,14 @@
+"""Divide Class"""
+from calc.calculations.calculation import Calculation
+
+
+class Divide(Calculation):
+    """divide calculation object"""
+    def get_result(self):
+        """get the divide results"""
+        result = 1.0
+        for value in self.values:
+
+        if self.value <= 0:
+            return "Error"
+        return self.value / result
