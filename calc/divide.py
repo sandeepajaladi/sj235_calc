6e694279b7e6bf6713de58ec5abad08447714215
sandeepajaladi <90530329+sandeepajaladi@users.noreply.github.com>
2021-11-16 11:08:48 -0500

    Update divide.py

--- calc/divide.py
+++ calc/divide.py
@@ -6,8 +6,6 @@ class Divide(Calculation):
     """Divide class has a method to get the result of calculation from the parent class"""
     def get_result(self):
         """Returns result from the function"""
-        result = 1.0
-        for value in self.values:
-            if self.value <= 0:
-                return "Error"
-            return self.value / result
+        if self.value_b <= 0:
+            return "Error"
+        return self.value_a / self.value_b
