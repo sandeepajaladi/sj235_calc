33490c2e8fef3cd40e581d1d20bf188f1a176bbd
jaladis <sj235@njit.edu>
2021-11-16 01:29:04 -0500

    Updating  branch for assignment 4 with corrections

--- tests/multiplication_test.py
+++ tests/multiplication_test.py
@@ -1,11 +1,11 @@
 """Testing Subtraction"""
-from calc.calculations.multiplication import Multiplication
+from calc.calculations.multiply import Multiply
 
 def test_calculation_subtraction():
     """testing that our calculator has a static method for addition"""
     #Arrange
     mynumbers = (1.0, 2.0)
-    multiplication = Mutiplication(mynumbers)
+    multiply = Mutiply(mynumbers)
     #Act
     #Assert
-    assert multiplication.get_result() == 2
+    assert multiply.get_result() == 2
